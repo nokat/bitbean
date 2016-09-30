24e9171956ee74bf65dcd317d8d2ee66ea32ea6a
Shawn Kent <admin@bitbean.org>
2016-09-30 13:12:02 -0400

    Changed progress bar to time based rather than blocks

--- src/qt/bitbeangui.h
+++ src/qt/bitbeangui.h
@@ -103,6 +103,8 @@ private:
     RPCConsole *rpcConsole;
 
     QMovie *syncIconMovie;
+    /** Keep track of previous number of blocks, to detect progress */
+    int prevBlocks;
 
     uint64_t nWeight;
 
