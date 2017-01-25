80efe807dba830e1eaa1b1f4347dc38787431c50
Shawn Kent <admin@bitbean.org>
2017-01-25 14:23:21 -0500

    Add GetTimeMicros() for more accurate benchmarks Add RPC command, ping (with pong):  new getpeerinfo <pingtime> results and <pingwait> fields to show lagging peers
    Modify ping automatically every 2min., instead of after 30min. of no sending (latency & keep alive)
    Disconnect if no reply after 20min. instead of 90min.

--- src/bitbeanrpc.cpp
+++ src/bitbeanrpc.cpp
@@ -242,6 +242,7 @@ static const CRPCCommand vRPCCommands[] =
     { "getblockcount",          &getblockcount,          true,   false },
     { "getconnectioncount",     &getconnectioncount,     true,   false },
     { "getpeerinfo",            &getpeerinfo,            true,   false },
+    { "ping",                   &ping,                   true,   false },
     { "addnode",                &addnode,                true,   true  },
     { "getaddednodeinfo",       &getaddednodeinfo,       true,   true  },
     { "getdifficulty",          &getdifficulty,          true,   false },
